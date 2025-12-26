6186fa6b543e4c4f87667c3fa483df4b64f976af
Xiaohong Zhou <1614103587@qq.com>
2025-12-26 16:29:43 +0800

    Initial Commit

---
+++ WindWhisper/WindWhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindWhisperApp.swift
+//  WindWhisper
+//
+//  Created by 周晓红 on 2025/12/26.
+//
+
+import SwiftUI
+
+@main
+struct WindWhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
